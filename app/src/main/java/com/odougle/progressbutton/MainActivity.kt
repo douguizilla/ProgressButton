6d407f2b2a47ffb4b4e7f1a8bd89cbd6362d5327
douguizilla <douglasgommez@gmail.com>
2021-07-26 12:28:16 -0300

    add action afterward click on button. Need to fix selector, the button isn`t get grey on loanding state

--- app/src/main/java/com/odougle/progressbutton/MainActivity.kt
+++ app/src/main/java/com/odougle/progressbutton/MainActivity.kt
@@ -2,10 +2,23 @@ package com.odougle.progressbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.odougle.progressbutton.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.progressButton.setOnClickListener {
+            binding.progressButton.setLoading()
+            lifecycleScope.launch {
+                delay(3_000)
+                binding.progressButton.setNormal()
+            }
+        }
     }
 }
