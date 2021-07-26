852dc6c23d277f0d8bc16acb3931d857e0834aeb
douguizilla <douglasgommez@gmail.com>
2021-07-26 11:14:57 -0300

    create ProgressButton.kt to instance button programmatically

---
+++ app/src/main/java/com/odougle/progressbutton/ProgressButton.kt
@@ -0,0 +1,12 @@
+package com.odougle.progressbutton
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ProgressButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0
+): ConstraintLayout(context, attrs, defStyleAttr) {
+}
